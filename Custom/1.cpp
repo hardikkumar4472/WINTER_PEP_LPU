667221e1c8ee232949d83705680500fef5c4692c
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-01-27 21:16:23 +0530

    Add initial implementation for anagram transformation problem and configuration files

---
+++ Custom/1.cpp
@@ -0,0 +1,21 @@
+// Problem statement:
+//       You are given two string s and t you have to find what 
+//       is minimum changes required in t string to make it 
+//       anagram of string s
+//       1<=s.length(),t.length()<=2*10^5
+//       example1:  
+//       s="aaba" t="abba"  ans = 1     
+//       s="axbxa" t="aaaxb" ans = 1
+
+ 
+#include<bits/stdc++.h> 
+using namespace std;
+int main(){
+    string s;
+    string t;
+    cin>>s;
+    cin.ignore();
+    cin>>t;
+    map<int> mpt;
+    map<int> mpt;
+}

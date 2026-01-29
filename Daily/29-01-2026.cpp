7ca2218afc31d6369862e5c39dd4665bbeb433b8
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-01-29 12:14:27 +0530

    29th Jan Updated

---
+++ Daily/29-01-2026.cpp
@@ -0,0 +1,8 @@
+/*
+#Problem 1: https://leetcode.com/problems/maximum-tastiness-of-candy-basket/
+#Problem 2: https://leetcode.com/problems/minimized-maximum-of-products-distributed-to-any-store/description/
+*/
+
+/*
+#Two Pointer
+*/

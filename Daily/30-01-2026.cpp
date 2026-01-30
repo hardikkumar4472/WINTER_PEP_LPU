780e97c319959984ef9fedcd18a9271a105e8c17
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-01-30 15:22:38 +0530

    Aggressive cows solution updated

--- Daily/30-01-2026.cpp
+++ Daily/30-01-2026.cpp
@@ -1,4 +1,5 @@
 /*
 Problem 1: https://www.geeksforgeeks.org/problems/aggressive-cows/1
+solution: https://onlinegdb.com/oXh-IPBZT
 Problem 2: https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/ (Amazon OA)
 */
